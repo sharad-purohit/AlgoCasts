0756a1df17037b001eb5224ad3194a9f4df71e1c
Sharad Purohit <sharadpurohit777@gmail.com>
2020-09-20 16:27:20 +0530

    Anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,64 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function getCleanStr(str) {
+  return str.replace(/[^\w]/, '');
+}
+
+function anagrams(stringA, stringB) {
+  // Get cleaned up string to remove puntuations and spaces
+  const cleanStringA = getCleanStr(stringA).toLowerCase();
+  const cleanStringB = getCleanStr(stringB).toLowerCase();
+
+  // Sort charachters strings
+  const strASorted = cleanStringA.split('').sort().join('');
+  const strBSorted = cleanStringB.split('').sort().join('');
+
+  return strASorted === strBSorted;
+}
 
 module.exports = anagrams;
+// function anagrams(stringA, stringB) {
+//   // Get cleaned up string to remove puntuations and spaces
+//   const cleanStringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//   const cleanStringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+//   // Return false if cleanup strings are not of equal length
+//   if (cleanStringA.length !== cleanStringB.length) {
+//     return false;
+//   }
+//   const stringACharMap = getCharCountMapFromString(cleanStringA);
+//   const stringBCharMap = getCharCountMapFromString(cleanStringB);
+//   let isAnagram = true;
+//   for(let char in stringACharMap) {
+//     if(stringACharMap[char] !== stringBCharMap[char]) {
+//       isAnagram = false;
+//     }
+//     break;
+//   }
+//   return isAnagram;
+// }
+
+
+//
+// function getCharCountMapFromString(str) {
+//   return str.split('').reduce((map, curr) => {
+//     return {
+//       ...map,
+//       [curr]: map[curr] + 1 || 1
+//     }
+//   }, {})
+// }
+//
+// function anagrams(stringA, stringB) {
+//   // Get cleaned up string to remove puntuations and spaces
+//   const cleanStringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//   const cleanStringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+//   // Return false if cleanup strings are not of equal length
+//   if (cleanStringA.length !== cleanStringB.length) {
+//     return false;
+//   }
+//   const stringACharMap = getCharCountMapFromString(cleanStringA);
+//   const stringBCharMap = getCharCountMapFromString(cleanStringB);
+//   let isAnagram = true;
+//   return Object.keys(stringACharMap).every((char) => stringACharMap[char] === stringBCharMap[char]);
+// }

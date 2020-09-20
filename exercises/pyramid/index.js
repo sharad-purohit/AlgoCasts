b78b6e82d567aa024102144378b8270246406ab2
Sharad Purohit <sharadpurohit777@gmail.com>
2020-09-20 18:12:21 +0530

    Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  const noOfCols = 2*n - 1;
+  const mid = Math.floor(noOfCols / 2);
+  const levelLength = level.length;
+  if(n === row) {
+    return;
+  }
+  if ( levelLength === noOfCols) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  if(mid - row <= levelLength && mid + row >= levelLength) {
+    level += '#';
+    pyramid(n, row, level);
+  } else {
+    level += ' ';
+    pyramid(n, row, level);
+  }
+}
 
 module.exports = pyramid;
+//
+//
+// function pyramid(n) {
+//   const mid = Math.floor(( 2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = ''
+//
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (mid - row <= col && mid + row >= col) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
